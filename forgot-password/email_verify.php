ead6883acd17a1d213e53e75599430fb8bb2326f
Angela Mae Salvaña <120080231+AngelaMaeSalvana@users.noreply.github.com>
2024-02-14 08:21:16 +0800

    Update

--- forgot-password/email_verify.php
+++ forgot-password/email_verify.php
@@ -37,12 +37,12 @@
                     <p>A verification code has been sent to your email address.</p>
                     <form method="POST">    
                     <div class="input-con">
-                        <input type="number" maxlength="1">
-                        <input type="number" disabled maxlength="1">
-                        <input type="number" disabled maxlength="1">
-                        <input type="number" disabled maxlength="1">
-                        <input type="number" disabled maxlength="1">
-                        <input type="number" disabled maxlength="1">
+                        <input type="number" >
+                        <input type="number" disabled >
+                        <input type="number" disabled >
+                        <input type="number" disabled >
+                        <input type="number" disabled >
+                        <input type="number" disabled >
                     </div>
                     <br/> 
                     <div class="btn-container row">
@@ -62,7 +62,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"> </script>
 <script>
-    
+
 </script>
  
 </body>
