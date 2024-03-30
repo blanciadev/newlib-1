6ab3a2c5343bfd29eedc9fafa6ae5ed10e15b83b
Kurt Bernstein P. Blancia <k.blancia.dev@gmail.com>
2024-03-30 12:44:46 +0700

    staff fines ui

--- staff/staff_fines.php
+++ staff/staff_fines.php
@@ -3,53 +3,7 @@
 
 session_start();
 
-
-
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VillaReadHub - Log Record</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="./staff.css" rel="stylesheet">
-    <link rel="icon" href="../images/lib-icon.png ">
-</head>
-<body>
-    <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" ><!--sidenav container-->
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <h2>Villa<span>Read</span>Hub</h2> 
-            <img src="../images/lib-icon.png" style="width: 45px;" alt="lib-icon"/>
-        </a><!--header container-->
-        <div class="user-header mt-4 d-flex flex-row flex-wrap align-content-center justify-content-evenly"><!--user container-->
-            <img src="https://github.com/mdo.png" alt="" width="50" height="50" class="rounded-circle me-2">
-            <strong><span><?php echo $_SESSION["staff_name"] ."<br/>"; echo $_SESSION["role"]; ?></span> </strong> 
-        </div>
-        <hr>
-        <ul class="nav nav-pills flex-column mb-auto"><!--navitem container-->
-            <li class="nav-item"> <a href="./staff_dashboard.php" class="nav-link link-body-emphasis " > <i class='bx bxs-home'></i>Dashboard </a> </li>
-            <li class="nav-item"> <a href="./staff_books.php" class="nav-link link-body-emphasis"><i class='bx bxs-book'></i>Books</a> </li>
-            <li class="nav-item"> <a href="./staff_transaction_dash.php" class="nav-link link-body-emphasis"><i class='bx bxs-customize'></i>Transaction</a> </li>
-           <li class="nav-item"> <a href="./staff_log.php" class="nav-link link-body-emphasis"><i class='bx bxs-user-detail'></i>Log Record</a> </li>
-            <li class="nav-item active"> <a href="./staff_fines.php" class="nav-link link-body-emphasis"><i class='bx bxs-wallet'></i>Fines</a> </li>
-            <hr>
-            <li class="nav-item"> <a href="./staff_settings.php" class="nav-link link-body-emphasis"><i class='bx bxs-cog'></i>Settings</a> </li>
-            <li class="nav-item"> <a href="../logout.php" class="nav-link link-body-emphasis"><i class='bx bxs-wallet'></i>Log Out</a> </li>
-        </ul>
-    </div>
-
-    <div class="board container"><!--board container-->
-
-        <?php
-        
-        
+       
 
 // Database connection
 $conn = mysqli_connect("localhost", "root", "root", "db_library_2", 3308); 
@@ -90,50 +44,86 @@ ON
 "; 
 $result = mysqli_query($conn, $query);
 
-// Check if there are any records
-if(mysqli_num_rows($result) > 0) {
-    echo '<table class="table">';
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th scope="col">Visitors Id "</th>';
-    echo '<th scope="col">Borrower Id "</th>';
-    echo '<th scope="col">First Name</th>';
-    echo '<th scope="col">Middle Name</th>';
-    echo '<th scope="col">Last Name</th>';
-    echo '<th scope="col">Contact Number</th>';
-   
-    echo '<th scope="col">Fines</th>';
-   
-    echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
 
-    // Loop through each row in the result set
-    while($row = mysqli_fetch_assoc($result)) {
-        echo '<tr>';
-        echo '<td>' . $row['Borrower_ID'] . '</td>'; 
-        echo '<td>' . $row['First_Name'] . '</td>'; 
-        echo '<td>' . $row['Middle_Name'] . '</td>'; 
-        echo '<td>' . $row['Last_Name'] . '</td>'; 
-        echo '<td>' . $row['Contact_Number'] . '</td>'; 
-      
-        echo '<td>' . $row['affiliation'] . '</td>'; 
-        echo '<td>' . $row['Amount'] . '</td>'; 
-        echo '</tr>';
-    }
-
-    echo '</tbody>';
-    echo '</table>';
-} else {
-    echo 'No log records found.';
-}
-
-
-// Close the database connection
-mysqli_close($conn);
-        ?>
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VillaReadHub - Log Record</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="./staff.css" rel="stylesheet">
+    <link rel="icon" href="../images/lib-icon.png ">
+</head>
+<body>
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" ><!--sidenav container-->
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <h2>Villa<span>Read</span>Hub</h2> 
+            <img src="../images/lib-icon.png" style="width: 45px;" alt="lib-icon"/>
+        </a><!--header container-->
+        <div class="user-header mt-4 d-flex flex-row flex-wrap align-content-center justify-content-evenly"><!--user container-->
+            <img src="https://github.com/mdo.png" alt="" width="50" height="50" class="rounded-circle me-2">
+            <strong><span><?php echo $_SESSION["staff_name"] ."<br/>"; echo $_SESSION["role"]; ?></span> </strong> 
+        </div>
+        <hr>
+        <ul class="nav nav-pills flex-column mb-auto"><!--navitem container-->
+            <li class="nav-item"> <a href="./staff_dashboard.php" class="nav-link link-body-emphasis " > <i class='bx bxs-home'></i>Dashboard </a> </li>
+            <li class="nav-item"> <a href="./staff_books.php" class="nav-link link-body-emphasis"><i class='bx bxs-book'></i>Books</a> </li>
+            <li class="nav-item"> <a href="./staff_transaction_dash.php" class="nav-link link-body-emphasis"><i class='bx bxs-customize'></i>Transaction</a> </li>
+           <li class="nav-item"> <a href="./staff_log.php" class="nav-link link-body-emphasis"><i class='bx bxs-user-detail'></i>Log Record</a> </li>
+            <li class="nav-item active"> <a href="./staff_fines.php" class="nav-link link-body-emphasis"><i class='bx bxs-wallet'></i>Fines</a> </li>
+            <hr>
+            <li class="nav-item"> <a href="./staff_settings.php" class="nav-link link-body-emphasis"><i class='bx bxs-cog'></i>Settings</a> </li>
+            <li class="nav-item"> <a href="../logout.php" class="nav-link link-body-emphasis"><i class='bx bxs-wallet'></i>Log Out</a> </li>
+        </ul>
     </div>
+
+    
+<div class="container"><!--board container-->
+<div class="scrollable-table" style="max-height: 900px; overflow-y:auto;">
+    <?php
+    // Your existing PHP code to fetch and display table data
+    ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Visitors Id</th>
+                <th scope="col">Borrower Id</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Middle Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Contact Number</th>
+                <th scope="col">Fines</th>
+                <th scope="col">Date Borrowed</th>
+                <th scope="col">Due Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Loop through each row in the result set
+            while($row = mysqli_fetch_assoc($result)) {
+                echo '<tr>';
+                echo '<td>' . $row['Borrower_ID'] . '</td>'; 
+                echo '<td>' . $row['First_Name'] . '</td>'; 
+                echo '<td>' . $row['Middle_Name'] . '</td>'; 
+                echo '<td>' . $row['Last_Name'] . '</td>'; 
+                echo '<td>' . $row['Contact_Number'] . '</td>'; 
+                echo '<td>' . $row['affiliation'] . '</td>'; 
+                echo '<td>' . $row['Amount'] . '</td>'; 
+                echo '<td>' . $row['Date_Borrowed'] . '</td>'; 
+                echo '<td>' . $row['Due_Date'] . '</td>'; 
+                echo '</tr>';
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+</div>
         
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"> </script>
