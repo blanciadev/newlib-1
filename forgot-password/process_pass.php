efea3f888e06f24134b89ade90407ad0f769a2a1
Angela Mae Salvaña <120080231+AngelaMaeSalvana@users.noreply.github.com>
2024-04-02 19:38:15 +0800

    Reset password UI + Added CSS

--- forgot-password/process_pass.php
+++ forgot-password/process_pass.php
@@ -66,42 +66,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VillaReadHub - Settings</title>
+    <title>Change Password</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="./staff.css" rel="stylesheet">
+    <link href="../styles.css" rel="stylesheet">
     <link rel="icon" href="../images/lib-icon.png ">
 </head>
 <body>
-   
+<div class="main-wrap container-fluid">
+        <div class="main-con row ">
+            <div class="img-sec col-7">
+                <img src="https://villanuevamisor.gov.ph/wp-content/uploads/2022/11/Villanueva-Municipal-Government-Association_LGU_Region-10-1024x692.jpg" alt="Library">
+            </div>
+            <div class="form-sec col-5">
+                <div class="title">
+                    <h1><strong>Villa<span>Read</span>Hub</strong></h1>
+                    <img src="../images/lib-icon.png" alt="lib-icon"/>
+                </div>
 
-    <div class="board container"><!--board container-->
-
-    <form id="userProfileForm" action="" method="post">
-  
-    <!-- Password Change Section -->
-    <div class="col-md-4">
-        <label for="oldPassword" class="form-label">Old Password</label>
-        <input type="password" class="form-control" id="oldPassword" name="oldPassword" required>
+                <div class="error-con"></div>
+                
+                <div class="form-con">
+                <form id="userProfileForm" action="" method="post">
+                    <!-- Password Change Section -->
+                    
+                    <div class="psw-container">
+                    <label for="newPassword">New Password</label>
+                    <input type="password"id="newPassword" name="newPassword" required>
+                    <br/><br/></div>
+                    
+                    <div class="psw-container">
+                        <label for="psw">Password</label>
+                        <input type="password"id="confirmPassword" name="confirmPassword" required>
+                        <br/>
+                    </div>
+                    <!-- Update button -->
+                    <button type="submit" class="btn">Update Password</button>
+                </form>
+                </div>
+        </div>
     </div>
-    <div class="col-md-4">
-        <label for="newPassword" class="form-label">New Password</label>
-        <input type="password" class="form-control" id="newPassword" name="newPassword" required>
-    </div>
-    <div class="col-md-4">
-        <label for="confirmPassword" class="form-label">Confirm Password</label>
-        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
-    </div>
-
-    <!-- Update button -->
-    <button type="submit" class="btn btn-primary">Update Password</button>
-    </form>
-
-    </div>
-        
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"> </script>
     <script> 
