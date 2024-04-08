79cfca82d2cb82772fa77cecf0d4658e6455e114
Angela Mae Salvaña <120080231+AngelaMaeSalvana@users.noreply.github.com>
2024-04-09 00:00:43 +0800

    added icons

--- staff/staff_books.php
+++ staff/staff_books.php
@@ -184,6 +184,7 @@ if (!isset($_SESSION["User_ID"]) || empty($_SESSION["User_ID"])) {
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"> </script>
     <script> 
+        //popover
         const popover = new bootstrap.Popover('.authorPop', {
             container: 'body'
             })
